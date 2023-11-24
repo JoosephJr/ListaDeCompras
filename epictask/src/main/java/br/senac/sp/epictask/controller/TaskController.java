826818d2a2c96e755d01966d862561aab36f6229
JoosephJr <jose.rlsjunior@gmail.com>
2023-11-24 10:46:15 -0300

    Commit Inicial

---
+++ epictask/src/main/java/br/senac/sp/epictask/controller/TaskController.java
@@ -0,0 +1,61 @@
+package br.senac.sp.epictask.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.senac.sp.epictask.model.Task;
+import br.senac.sp.epictask.repository.TaskRepository;
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("task")
+public class TaskController {
+
+    @Autowired
+    TaskRepository repository;
+
+    @GetMapping
+    public String index(Model model) {
+        var lista = repository.findAll();
+        model.addAttribute("tasks", lista);
+        return "task/index";
+    }
+
+    @GetMapping("new") // http://localhost:8080/task/new
+    public String form(Task task) {
+        return "task/form";
+    }
+
+    @PostMapping("new")
+    public String save(@Valid Task task, BindingResult result) {
+        if (result.hasErrors())
+            return "task/form";
+        repository.save(task);
+        return "redirect:/task";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteTask(@PathVariable Long id) {
+        repository.deleteById(id);
+        return "redirect:/task";
+
+    }
+
+    
+
+    @GetMapping("/detalhe/{id}")
+    public String receitaDetalhe(@PathVariable Long id, Model model) {
+        Task task = repository.findById(id).orElse(null);
+        model.addAttribute("task", task);
+        return "task/receita-detalhe";
+    }
+
+    
+
+}
